8706b762e90c5db7a567f29be56d12398377d844
ASH Walls <admin@arethawalls.com>
2019-02-04 12:23:39 -0700

    Got example template  + router working

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
+require('dotenv').config();
+
 const express = require('express');
+const path = require('path');
 const app = express();
+const PORT = process.env.PORT || '3000';
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 const nunjucks = require('nunjucks');
 nunjucks.configure('views', {
@@ -10,3 +15,10 @@ nunjucks.configure('views', {
     express: app
 });
 app.set('view engine', 'njk');
+
+const indexRouter = require('./routes/index');
+app.use('/', indexRouter);
+
+app.listen(PORT, () => {
+    console.log("Server listening on: http://localhost:" + PORT);
+})
